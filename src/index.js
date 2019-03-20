037db291578930a2624a6b87ce423f73243a7fb3
Natalitium <n.patorskaya@gmail.com>
2019-03-20 10:43:26 +0300

    Submit task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+ 
+  var lovers = [];
+  var counter = 0;
+  
+	for (var i = 1; i <= preferences.length; i++) {
+
+		let n = preferences[i - 1];
+		let m = preferences[n - 1];
+		let k = preferences[m - 1];
+   
+    let no_self_lovers = i != n && n != m && m != k;
+    let is_closed_triangle = k == i && no_self_lovers;
+    let was_not_detected_before = lovers.indexOf(i) < 0;
+  
+    if (is_closed_triangle && was_not_detected_before) {
+			lovers.push(n, m, k);
+			counter ++;
+		}
+	}
+	return counter;
 };
